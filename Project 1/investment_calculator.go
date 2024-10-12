9b894dc40e286272aab0ea3c9d36fb11257c8f10
Rengkat <successinmotion11@gmail.com>
2024-10-12 14:29:14 +0100

    use of const

--- Project 1/investment_calculator.go
+++ Project 1/investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate= 2.0
 	var investmentAmount float64 = 1000
 	 expectedReturnRate := 5.5
 	var year float64 = 10
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate / 100,year)
+	futureRealValue := futureValue/math.Pow(1+inflationRate/100,year)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
