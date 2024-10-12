6616b78dd727166b139e964e9a15968ab56a08c9
Rengkat <successinmotion11@gmail.com>
2024-10-12 13:57:18 +0100

    short cut assignment of variables

--- Project 1/investment_calculator.go
+++ Project 1/investment_calculator.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
+	 expectedReturnRate := 5.5
 	var year float64 = 10
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate / 100,year)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate / 100,year)
 	fmt.Println(futureValue)
 
 }
