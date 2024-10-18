6d11ae1ae981157825d79d3d70365ddc6a558185
Rengkat <successinmotion11@gmail.com>
2024-10-18 11:14:04 +0100

    working with functions

--- Project 2/profit_calculator.go
+++ Project 2/profit_calculator.go
@@ -3,22 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
 
-	printText("Revenue: ")
-	fmt.Scan(&revenue)
-
-	printText("Expenses: ")
-	fmt.Scan(&expenses)
-
-	printText("Tax rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt :=revenue - expenses
-	profit := ebt * (1- taxRate/100)
-	ratio := ebt/profit
+	revenue :=printText("Revenue: ")
+	expenses:=printText("Expenses: ")
+	taxRate:=printText("Tax rate: ")
+	
+	ebt, profit, ratio := calculateProfit(revenue,expenses,taxRate)
+	// ebt :=revenue - expenses
+	// profit := ebt * (1- taxRate/100)
+	// ratio := ebt/profit
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
@@ -26,12 +22,25 @@ func main() {
 	fmt.Printf(`the ration is: %.2f`,ratio) //2d.p-> use Printf with %
 	// Text(4,6)
 }
-func printText(txt string)  {
+func printText(txt string) float64  {
+	var userInput float64
 	fmt.Print(txt)
+	fmt.Scan(&userInput)
+	return userInput
 }
-func calculateProfit(revenue float64, expenses float64, taxRate float64)  {
+
+func calculateProfit(revenue float64, expenses float64, taxRate float64) (float64,float64,float64){
 	ebt :=revenue - expenses
 	profit := ebt * (1- taxRate/100)
-	return profit
+	ratio := ebt/profit
+return ebt, profit,ratio
 	
 }
+//alternatively
+// func calculateProfitAlt(revenue float64, expenses float64, taxRate float64) (ebt float64,profit float64,ratio float64){
+// 	ebt =revenue - expenses
+// 	profit = ebt * (1- taxRate/100)
+// 	ratio = ebt/profit
+// 	return
+	
+// }
